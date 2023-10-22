ff94d4f4128a207f6988a56e0918294d64498fec
Robusto <50712323+RBautista1292@users.noreply.github.com>
2023-10-22 13:46:05 -0600

    kjhsafkj

--- app/src/main/java/com/example/examen2doparcial2/PongGame.java
+++ app/src/main/java/com/example/examen2doparcial2/PongGame.java
@@ -1,7 +1,45 @@
 package com.example.examen2doparcial2;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
-public class PongGame extends View {
-    
+public class PongGame extends View implements SensorEventListener {
+    Paint pincel = new Paint();
+    int alto, ancho;
+    int tamanio = 40;
+    int borde = 12;
+    float ejeX = 0, ejeY = 0, ejeZ = 0;
+    String x, y, z;
+    public PongGame(Context context) {
+        super(context);
+        SensorManager smAdministrador = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
+        Sensor snsRotacion = smAdministrador.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        smAdministrador.registerListener(this, snsRotacion, SensorManager.SENSOR_DELAY_FASTEST);
+        Display pantalla = ((WindowManager) getContext() .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        ancho = pantalla.getWidth();
+        alto = pantalla.getHeight();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 }
